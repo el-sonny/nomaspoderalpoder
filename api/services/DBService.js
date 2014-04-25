4211739e85932f653bf5afac81306d0cf178acc3
urielaero <aero.uriel@gmail.com>
2014-04-25 00:32:58 -0500

    get avatars from twitter

--- api/services/DBService.js
+++ api/services/DBService.js
@@ -7,9 +7,32 @@ module.exports = {
 				console.log('Reps Procesados: %d',res.length);
 			});
 		});
-	}	
-};
+	}
+	, getAvatars: function(){
+		var  twitter = require('twitter') 
+		, twit = new twitter(sails.config.twitter_keys)
+		Representante.find().exec(function(err,rep){
+			rep.forEach(function(e,i){
+				if(e.twitter && !e['twitter_avatar'] ){
+					twit.get('/users/show.json',{screen_name:e.twitter},function(data){
+						console.log('response from %s',e.twitter,data);
+						if(data && data.profile_image_url)
+							Representante.update({id:e.id}
+							,{
+								twitter_avatar:data.profile_image_url
+							}
+							, function(err){
+								if(err) console.log(err);
+								console.log("update avatar %s",e.nombre);
+							});
+					});				
+				}
+			});
 
+		});
+	}
+};
+//Representante.find().done(function(e,r){console.log(r)});
 var extractEntidad = function(rep,callback){
 	Entidad.findOrCreate({nombre:rep.estado},{nombre:rep.estado},function(e,entidad){
 		if(e) return callback(e,entidad);
